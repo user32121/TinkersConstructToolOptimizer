3af0881c0d6a670af353b600f16c35548014c70d
jupiter <pengjupiter5@gmail.com>
2021-08-06 14:49:32 -0700

    added attack speed and dps calculations

--- optimize.js
+++ optimize.js
@@ -62,7 +62,7 @@ function Optimize()
 		if(bestBonusStat == -1)
 			display.appendChild(document.createTextNode(bestBaseStat));
 		else
-			display.appendChild(document.createTextNode(bestBaseStat+" + "+bestBonusStat));
+			display.appendChild(document.createTextNode(Number(bestBaseStat.toFixed(5))+" + "+Number(bestBonusStat.toFixed(5))));
 	}
 }
 
@@ -126,15 +126,38 @@ function selectToolPart(index)
 		}
 		else if(target == "attack-speed")
 		{
-			
+			let curStat = getAttackSpeed(curTool);
+			if(curStat > bestBaseStat+bestBonusStat)
+			{
+				bestBaseStat = getAttackSpeed(curTool, true);
+				bestBonusStat = curStat-bestBaseStat;
+				bestTool = curTool.slice(0);
+				if(useEmbossment)
+					bestEmbossment = embossment;
+			}
 		}
 		else if(target == "attack-damage")
 		{
-			
+			let curStat = getAttackDamage(curTool);
+			if(curStat > bestBaseStat+bestBonusStat)
+			{
+				bestBaseStat = getAttackDamage(curTool, true);
+				bestBonusStat = curStat-bestBaseStat;
+				bestTool = curTool.slice(0);
+				if(useEmbossment)
+					bestEmbossment = embossment;
+			}
 		}
 		else if(target == "dps")
 		{
-			
+			let curStat = getAttackDamage(curTool) * getAttackSpeed(curTool);
+			if(curStat > bestBaseStat)
+			{
+				bestBaseStat = curStat;
+				bestTool = curTool.slice(0);
+				if(useEmbossment)
+					bestEmbossment = embossment;
+			}
 		}
 		
 		return;
@@ -234,7 +257,7 @@ function getDurability(tool)
 	return Math.round(durability);
 }
 
-const miningSpeedIgnoredTraits = ["Aquadynamic","Autosmelt","Baconlicious","Beheading","Breakable","Cheap","Cheapskate","Cold-Blooded","Crude","Crude II","Duritae","Ecological","Enderference","Endspeed","Evil Aura","Experience Boost","Flammable","Fractured","Freezing","Hellish","Hovering","Insatiable","Jagged","Magically Brittle","Magically Modifiable","Magnetic","Magnetic II","Petramor","Prickly","Sharp","Slimey","Spiky","Splintering","Splitting","Squeaky","Stalwart","Superheat","Synergy","Tasty","Whispering","Writable","Writable II","Veiled"];
+const miningSpeedIgnoredTraits = ["Autosmelt","Baconlicious","Beheading","Breakable","Cheap","Cheapskate","Cold-Blooded","Crude","Crude II","Duritae","Ecological","Enderference","Endspeed","Evil Aura","Experience Boost","Flammable","Fractured","Freezing","Hellish","Hovering","Insatiable","Jagged","Magically Brittle","Magically Modifiable","Magnetic","Magnetic II","Petramor","Prickly","Sharp","Slimey","Spiky","Splintering","Splitting","Squeaky","Stalwart","Superheat","Synergy","Tasty","Whispering","Writable","Writable II","Veiled"];
 function getMiningSpeed(tool, baseStatOnly=false)
 {
 	let miningSpeed = [];
@@ -244,22 +267,20 @@ function getMiningSpeed(tool, baseStatOnly=false)
 	//base mining speed
 	for(let i = 0; i < toolParts.length; i++)
 		for(let part of toolParts[i].types)
+		{
 			if(part == "head")
 			{
 				miningSpeed.push(info.materials[tool[i]].head["mining speed"]);
 				miningLevel.push(info.materials[tool[i]].head["mining level"]);
-				for(trait of info.materials[tool[i]].head.traits)
-					traits.add(trait);
 			}
-			else if(part == "extra")
-				for(trait of info.materials[tool[i]].extra.traits)
-					traits.add(trait);
-			else if(part == "handle")
-				for(trait of info.materials[tool[i]].handle.traits)
-					traits.add(trait);
-			else if(part == "shaft"||part == "fletchling"||part == "bow"||part == "bowstring") {}  //no durability stat
+			else if(part == "extra"||part == "handle"||part == "shaft"||part == "fletchling"||part == "bow"||part == "bowstring") {}
 			else
 				throw "Not Implemented Exception (part "+JSON.stringify(part)+")";
+			
+			//traits
+			for(trait of info.materials[tool[i]][part].traits)
+				traits.add(trait);
+		}
 	
 	let avgMiningSpeed = avg(miningSpeed);
 	let toolSpeed = avgMiningSpeed;
@@ -280,9 +301,11 @@ function getMiningSpeed(tool, baseStatOnly=false)
 	let finalSpeed = toolSpeed;
 	
 	//traits
-	for(let partTypes of embossment.part.types)
-		for(let trait of info.materials[embossment.material][partTypes].traits)
-			traits.add(trait);
+	if(embossment != undefined)
+		for(let partTypes of embossment.part.types)
+			for(let trait of info.materials[embossment.material][partTypes].traits)
+				traits.add(trait);
+	console.log(traits);
 	for(trait of traits)
 	{
 		if(trait == "Alien")
@@ -290,6 +313,10 @@ function getMiningSpeed(tool, baseStatOnly=false)
 			finalSpeed += 1.862;
 		}
 		else if(trait == "Aridiculous")
+		{
+			//TODO: Advanced
+		}
+		else if(trait == "Aquadynamic")
 		{
 			//TODO: Advanced
 		}
@@ -330,12 +357,162 @@ function getMiningSpeed(tool, baseStatOnly=false)
 	return finalSpeed;
 }
 
-function getAttackSpeed(tool)
+const attackSpeedIgnoredTraits = ["Aquadynamic","Autosmelt","Baconlicious","Beheading","Breakable","Cheap","Cheapskate","Cold-Blooded","Crude","Crude II","Duritae","Ecological","Enderference","Endspeed","Evil Aura","Experience Boost","Flammable","Fractured","Freezing","Hellish","Hovering","Insatiable","Jagged","Magically Brittle","Magically Modifiable","Magnetic","Magnetic II","Petramor","Prickly","Sharp","Slimey","Spiky","Splintering","Splitting","Squeaky","Stalwart","Superheat","Synergy","Tasty","Whispering","Writable","Writable II","Veiled", "Twilit", "Alien", "Aridiculous", "Crumbling", "Momentum", "Precipitate", "Stonebound", "Unnatural"];
+function getAttackSpeed(tool, baseStatOnly=false)
 {
+	let attackSpeed = 1;
+	let traits = new Set();
 	
+	//get traits
+	for(let i = 0; i < toolParts.length; i++)
+		for(let part of toolParts[i].types)
+			for(trait of info.materials[tool[i]][part].traits)
+				traits.add(trait);
+	
+	let toolSpeed = attackSpeed;
+	
+	//tool modifiers
+	if(info.tools[selectedToolName].modifiers["attack speed"])
+		for(let [op, v] of info.tools[selectedToolName].modifiers["attack speed"])
+			if(op == "+")
+				toolSpeed += v;
+			else if(op == "*")
+				toolSpeed *= v;
+			else
+				throw "Not Implemented Exception ("+JSON.stringify(op)+")";
+	
+	if(baseStatOnly)
+		return toolSpeed;
+
+	let finalSpeed = toolSpeed;
+	
+	//traits
+	if(embossment != undefined)
+		for(let partTypes of embossment.part.types)
+			for(let trait of info.materials[embossment.material][partTypes].traits)
+				traits.add(trait);
+	for(trait of traits)
+	{
+		if(trait == "Lightweight")
+		{
+			finalSpeed *= 1.1;
+		}
+		else if(attackSpeedIgnoredTraits.includes(trait)) {}  //trait has no effect on mining speed
+		else
+			throw "Not Implemented Exception (trait "+JSON.stringify(trait)+")";
+	}
+	
+	return finalSpeed;
 }
 
-function getAttackDamage(tool)
+const attackDamageIgnoredTraits = ["Aquadynamic","Autosmelt","Baconlicious","Beheading","Breakable","Cheap","Cheapskate","Crumbling","Duritae","Ecological","Enderference","Endspeed","Evil Aura","Experience Boost","Flammable","Freezing","Hovering","Lightweight","Magically Brittle","Magically Modifiable","Magnetic","Magnetic II","Momentum","Petramor","Precipitate","Sharp","Slimey","Spiky","Splitting","Stalwart","Stonebound","Synergy","Tasty","Unnatural","Whispering","Writable","Writable II","Veiled"]
+function getAttackDamage(tool, baseStatOnly=false)
 {
+	let attackDamage = [];
+	let traits = new Set();
 	
+	//base attack damage
+	for(let i = 0; i < toolParts.length; i++)
+		for(let part of toolParts[i].types)
+		{
+			if(part == "head")
+				attackDamage.push(info.materials[tool[i]].head["attack damage"]);
+			else if(part == "extra"||part == "handle"||part == "shaft"||part == "fletchling"||part == "bow"||part == "bowstring") {}
+			else
+				throw "Not Implemented Exception (part "+JSON.stringify(part)+")";
+			
+			//traits
+			for(trait of info.materials[tool[i]][part].traits)
+				traits.add(trait);
+		}
+	
+	let avgAttackDamage = avg(attackDamage);
+	let toolDamage = avgAttackDamage;
+	
+	//tool modifiers
+	if(info.tools[selectedToolName].modifiers["attack damage"])
+		for(let [op, v] of info.tools[selectedToolName].modifiers["attack damage"])
+			if(op == "+")
+				toolDamage += v;
+			else if(op == "*")
+				toolDamage *= v;
+			else
+				throw "Not Implemented Exception ("+JSON.stringify(op)+")";
+	
+	if(baseStatOnly)
+		return toolDamage;
+
+	let finalDamage = toolDamage;
+	
+	//traits
+	if(embossment != undefined)
+		for(let partTypes of embossment.part.types)
+			for(let trait of info.materials[embossment.material][partTypes].traits)
+				traits.add(trait);
+	for(trait of traits)
+	{
+		if(trait == "Alien")
+		{
+			finalDamage += 1.33;
+		}
+		else if(trait == "Aridiculous")
+		{
+			//TODO: Advanced
+		}
+		else if(trait == "Cold-Blooded")
+		{
+			//TODO: Advanced
+		}
+		else if(trait == "Crude")
+		{
+			finalDamage += toolDamage*0.05;
+		}
+		else if(trait == "Crude II")
+		{
+			finalDamage += toolDamage*0.1;
+		}
+		else if(trait == "Fractured")
+		{
+			finalDamage += 1.5;
+		}
+		else if(trait == "Hellish")
+		{
+			//TODO: Advanced
+		}
+		else if(trait == "Insatiable")
+		{
+			finalDamage += 10/3;
+		}
+		else if(trait == "Jagged")
+		{
+			finalDamage += Math.log((getDurability(tool)-1) / 72 + 1) * 2;
+		}
+		else if(trait == "Prickly")
+		{
+			finalDamage += 1;
+		}
+		else if(trait == "Splintering")
+		{
+			finalDamage += 0.3 * (5+1);
+		}
+		else if(trait == "Squeaky")
+		{
+			finalDamage = 0;
+			toolDamage = 0;
+		}
+		else if(trait == "Superheat")
+		{
+			//TODO: Advanced
+			//finalDamage += toolDamage * 0.35;
+		}
+		else if(trait == "Twilit")
+		{
+			//TODO: Advanced
+		}
+		else if(attackDamageIgnoredTraits.includes(trait)) {}  //trait has no effect on mining speed
+		else
+			throw "Not Implemented Exception (trait "+JSON.stringify(trait)+")";
+	}
+	
+	return finalDamage;
 }
